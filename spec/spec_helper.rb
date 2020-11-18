31f72ca1e21a99987703d6fb5cf044ccae303bb3
Dmitry Tsepelev <dmitry.a.tsepelev@gmail.com>
2020-11-18 10:44:03 +0300

    Migrate to GitHub Actions (#45)
    * Migrate to GitHub Actions
    
    * Stop Rails 4.2 support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,23 @@
 
 ENV["RAILS_ENV"] = "test"
 
-require "coveralls"
-Coveralls.wear!
+if ENV["COVERALLS"]
+  require "simplecov"
+  require "simplecov-lcov"
+
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = "coverage/lcov.info"
+  end
+
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+    [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::LcovFormatter]
+  )
+
+  SimpleCov.start do
+    add_filter "spec/"
+  end
+end
 
 require_relative "dummy/config/environment"
 
