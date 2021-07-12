5c3d99e4fb01b6207048c34ab042b69683652f91
Taylor Thurlow <thurlow@hey.com>
2021-07-12 16:01:02 -0700

    Remove Rails direct dependency
    Requires calling `ArLazyPreload.install_hooks` in non-Rails
    environments.

--- lib/ar_lazy_preload.rb
+++ lib/ar_lazy_preload.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 
 require "ar_lazy_preload/configuration"
-require "ar_lazy_preload/railtie"
+require "ar_lazy_preload/railtie" if defined?(::Rails)
+
+require "ar_lazy_preload/active_record/base"
+require "ar_lazy_preload/active_record/relation"
+require "ar_lazy_preload/active_record/association"
+require "ar_lazy_preload/active_record/collection_association"
+require "ar_lazy_preload/active_record/merger"
+require "ar_lazy_preload/active_record/association_relation"
+require "ar_lazy_preload/active_record/collection_proxy"
 
 module ArLazyPreload
   class << self
     def config
       @config ||= Configuration.new
+    end
+
+    def install_hooks
+      ActiveRecord::Base.include(Base)
+
+      ActiveRecord::Relation.prepend(Relation)
+      ActiveRecord::AssociationRelation.prepend(AssociationRelation)
+      ActiveRecord::Relation::Merger.prepend(Merger)
+
+      [
+        ActiveRecord::Associations::CollectionAssociation,
+        ActiveRecord::Associations::Association
+      ].each { |klass| klass.prepend(Association) }
+
+      ActiveRecord::Associations::CollectionAssociation.prepend(CollectionAssociation)
+      ActiveRecord::Associations::CollectionProxy.prepend(CollectionProxy)
+
+      ArLazyPreload::Preloader.patch_for_rails_7! if ActiveRecord::VERSION::MAJOR >= 7
     end
   end
 end
