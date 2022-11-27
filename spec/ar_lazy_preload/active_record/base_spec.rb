0839f654b52180e0306793b824b8eaf7d34d4eb0
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2022-11-27 21:31:49 +0400

    Add spec to make sure .reload does not affect the context

--- spec/ar_lazy_preload/active_record/base_spec.rb
+++ spec/ar_lazy_preload/active_record/base_spec.rb
@@ -14,4 +14,16 @@ describe ArLazyPreload::Base do
 
     it { is_expected.to respond_to(:skip_preload) }
   end
+
+  describe "#reload" do
+    let!(:user) { create(:user) }
+
+    it "not looses context" do
+      user = User.lazy_preload(posts: :user).first
+
+      user.reload.lazy_preload_context.tap do |context|
+        expect(context).not_to be_nil
+      end
+    end
+  end
 end
